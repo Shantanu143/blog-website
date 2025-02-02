cda829444b57ffaac01d3549678fa68c8d091c6a
ShAnTaNu <shantanunirapal@gmail.com>
2025-02-02 22:03:43 +0530

    redux setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,29 @@
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/counter";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
-export default App
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- server/model/userModel.js
+++ server/model/userModel.js
@@ -4,6 +4,9 @@ const UserSchema = new mongoose.Schema(
   {
     name: { type: String, require: true },
     email: { type: String, require: true },
+    password: { type: String, require: true },
+    subscriptionPlan: { type: String, require: true },
+    paymentStatus: { type: String, require: true },
   },
   { timestamps: true }
 );

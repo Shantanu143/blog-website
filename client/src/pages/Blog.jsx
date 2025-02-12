6cdb35a4b1783282488da34c88ac5699b95905ff
Shantanu143 <shantanunirapal@gmail.com>
2025-02-12 15:41:48 +0530

    blog page done

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,11 +1,15 @@
 import AllPosts from "../components/AllPosts";
 import BlogHeader from "../components/BlogHeader";
+import Catagory from "../components/Catagory";
+import JoinOurTeam from "../components/JoinOurTeam";
 
 const Blog = () => {
   return (
     <>
       <BlogHeader />
       <AllPosts />
+      <Catagory />
+      <JoinOurTeam />
     </>
   );
 };

1289333f737ea4d118b1fcd87a718bc6b3f315af
ShAnTaNu <shantanunirapal@gmail.com>
2025-02-14 21:31:53 +0530

    all the changes done

---
+++ client/src/components/JoinTeam.jsx
@@ -0,0 +1,18 @@
+const JoinTeam = () => {
+  return (
+    <div className="mx-10 my-20 flex flex-col justify-center items-center">
+      <h2 className="h2 m-4 text-center">
+        Join our team to be a part <br /> of our story
+      </h2>
+      <p className="body-1 m-4 text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br /> sed do
+        eiusmod tempor incididunt.
+      </p>
+      <button className="px-6 py-2 m-4 font-semibold bg-[#FFD050]">
+        Join Now
+      </button>
+    </div>
+  );
+};
+
+export default JoinTeam;
